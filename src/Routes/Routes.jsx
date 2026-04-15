1e95f30ae4b76cb325bdb479c3727bc2b652c342
shahariar ahammad fahim <fahim789m@gmail.com>
2026-04-15 22:05:04 +0600

    Add navbar, homepage, and main layout components

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { Children } from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import HomePage from "../Pages/HomePage/HomePage";
+import MainLayout from "../Layout/mainLayout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
+    element: <MainLayout />,
+
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        
+      }
+    ],
   },
 ]);
 
 const root = document.getElementById("root");
 
-ReactDOM.createRoot(root).render(
-  <RouterProvider router={router} />,
-);
+ReactDOM.createRoot(root).render(<RouterProvider router={router} />);
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router";
 import "./index.css";
 import { router } from "./Routes/Routes";
-import { StrictMode } from "react";
 
 const root = document.getElementById("root");
 
-ReactDOM.createRoot(root).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-);
+ReactDOM.createRoot(root).render(<RouterProvider router={router} />);
